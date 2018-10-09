ac24968ccb50ffb92eb77ebcfd7ec3b45bc9eefe
Celestial Doom <ubuntu.achromic@gmail.com>
2018-10-09 23:03:23 +0100

    SlimShopper UWP Beta - Is an Unofficial app to access the Amazon shopping site. Originally named 'The Unofficial Amazon App', name change came about to forego any name issues with a certain shopping portal. This (as the title says) is the Beta version of my existing app.

---
+++ SlimShopper/MainPage.xaml.vb
@@ -0,0 +1,199 @@
+﻿' The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+Imports Windows.Phone.UI.Input
+
+''' <summary>
+''' An empty page that can be used on its own or navigated to within a Frame.
+''' </summary>
+''' <seealso cref="Windows.UI.Xaml.Controls.Page" />
+''' <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector" />
+''' <seealso cref="Windows.UI.Xaml.Markup.IComponentConnector2" />
+Public NotInheritable Class MainPage
+    Inherits Page
+
+    ''' <summary>
+    ''' Backs the pressed.
+    ''' </summary>
+    ''' <param name="sender">The sender.</param>
+    ''' <param name="e">The <see cref="BackPressedEventArgs"/> instance containing the event data.</param>
+    Async Sub BackPressed(sender As Object, e As BackPressedEventArgs)
+        'Handles any Back button presses.
+        e.Handled = True
+        If SSWV.CanGoBack Then
+            SSWV.GoBack()
+        Else
+            Await displayMessageAsync("Quit " & AppName, "Are you sure you want to quit the app?", "")
+        End If
+        _G_ABOUT.Visibility = Visibility.Collapsed
+        _G_COUNTRY.Visibility = Visibility.Collapsed
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the _ACLOSE control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Sub _ACLOSE_Click(sender As Object, e As RoutedEventArgs) Handles _ACLOSE.Click
+        _G_ABOUT.Visibility = Visibility.Collapsed
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the _CCLOSE control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Sub _CCLOSE_Click(sender As Object, e As RoutedEventArgs) Handles _CCLOSE.Click
+        _G_COUNTRY.Visibility = Visibility.Collapsed
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the ABOUT control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Sub ABOUT_Click(sender As Object, e As RoutedEventArgs) Handles ABOUT.Click
+        PivotSettingsAbout.SelectedIndex = 0
+        Dim number As PackageVersion = Package.Current.Id.Version
+        version.Text = String.Format(" {0}.{1}.{2}" & vbCrLf, number.Major, number.Minor, number.Build)
+        abouttext.Text = AboutInfo
+        privacy.Text = PrivacyInfo
+        ScrollViewAbout.ChangeView(Nothing, 0, Nothing, True)
+        ScrollViewPrivacy.ChangeView(Nothing, 0, Nothing, True)
+        _G_ABOUT.Visibility = Visibility.Visible
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the ACCOUNT control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Sub ACCOUNT_Click(sender As Object, e As RoutedEventArgs) Handles ACCOUNT.Click
+        Dim selectedValue As Integer
+        selectedValue = localSettings.Values("STORED_COUNTRY")
+        SSWV.Source = New Uri("https://www.amazon." & CountryURL(selectedValue) & "/gp/css/homepage.html/ref=nav_youraccount_ya")
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the BACK control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Async Sub BACK_Click(sender As Object, e As RoutedEventArgs) Handles BACK.Click
+        If SSWV.CanGoBack Then
+            SSWV.GoBack()
+        Else
+            Await displayMessageAsync("Quit " & AppName, "Are you sure you want to quit the app?", "")
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the HOME control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Sub HOME_Click(sender As Object, e As RoutedEventArgs) Handles HOME.Click
+        Dim selectedValue As Integer
+        selectedValue = localSettings.Values("STORED_COUNTRY")
+        SSWV.Source = New Uri("https://www.amazon." & CountryURL(selectedValue) & "/")
+        iconRotation.Begin()
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the hyperDev control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Async Sub hyperDev_Click(sender As Object, e As RoutedEventArgs) Handles hyperDev.Click
+        Dim DevURL = New Uri("https://github.com/CelestialDoom/")
+        Await Windows.System.Launcher.LaunchUriAsync(DevURL)
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the hyperLogo control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Async Sub hyperLogo_Click(sender As Object, e As RoutedEventArgs) Handles hyperLogo.Click
+        Dim logoURL = New Uri("http://www.iconarchive.com/show/ios7-icons-by-icons8/Ecommerce-Shopping-Cart-Empty-icon.html")
+        Await Windows.System.Launcher.LaunchUriAsync(logoURL)
+    End Sub
+
+    ''' <summary>
+    ''' Handles the SelectionChanged event of the lstCountry control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="SelectionChangedEventArgs"/> instance containing the event data.</param>
+    Private Sub lstCountry_SelectionChanged(sender As Object, e As SelectionChangedEventArgs) Handles lstCountry.SelectionChanged
+        Dim selectedindex As Integer
+        Dim selectedValue As Integer
+        selectedindex = lstCountry.SelectedIndex
+        If selectedindex > -1 Then
+            _G_COUNTRY.Visibility = Visibility.Collapsed
+            Dim value As Object = localSettings.Values("STORED_COUNTRY")
+            localSettings.Values("STORED_COUNTRY") = selectedindex
+            selectedValue = localSettings.Values("STORED_COUNTRY")
+            SSWV.Source = New Uri("https://www.amazon." & CountryURL(selectedValue) & "/")
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Loaded event of the MainPage control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Sub MainPage_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        lstCountry.ItemsSource = LoadCountries()
+        _G_COUNTRY.Visibility = Visibility.Collapsed
+        AddHandler HardwareButtons.BackPressed, AddressOf BackPressed
+        If Stored_ID Is Nothing Then
+            'localSettings.Values("STORED_COUNTRY") = "0"
+            _G_COUNTRY.Visibility = Visibility.Visible
+        Else
+            CountryValue = Stored_ID
+        End If
+        Dim value As Object = localSettings.Values("STORED_COUNTRY")
+        URLBuilder = "https://www.amazon." & CountryURL(value) & "/"
+        iconRotation.Begin()
+        SSWV.Source = New Uri("https://www.amazon." & CountryURL(value) & "/")
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the ORDERS control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Sub ORDERS_Click(sender As Object, e As RoutedEventArgs) Handles ORDERS.Click
+        Dim selectedValue As Integer
+        selectedValue = localSettings.Values("STORED_COUNTRY")
+        SSWV.Source = New Uri("https://www.amazon." & CountryURL(selectedValue) & "/gp/css/order-history/ref=nav_youraccount_order")
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the QUIT control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Sub QUIT_Click(sender As Object, e As RoutedEventArgs) Handles QUIT.Click
+        Application.Current.Exit()
+    End Sub
+
+    ''' <summary>
+    ''' Handles the LoadCompleted event of the SSWV control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="NavigationEventArgs"/> instance containing the event data.</param>
+    Private Sub SSWV_LoadCompleted(sender As Object, e As NavigationEventArgs) Handles SSWV.LoadCompleted
+        iconRotation.Stop()
+    End Sub
+
+    ''' <summary>
+    ''' Handles the Click event of the TOP control.
+    ''' </summary>
+    ''' <param name="sender">The source of the event.</param>
+    ''' <param name="e">The <see cref="RoutedEventArgs"/> instance containing the event data.</param>
+    Private Async Sub TOP_Click(sender As Object, e As RoutedEventArgs) Handles TOP.Click
+        Dim ScrollToTopString = "var int = setInterval(function(){window.scrollBy(0, -36); if( window.pageYOffset === 0 ) clearInterval(int); }, 0.0);"
+        Await SSWV.InvokeScriptAsync("eval", New String() {ScrollToTopString})
+    End Sub
+
+End Class
---
+++ SlimShopper/Vars.vb
@@ -0,0 +1,47 @@
+﻿Imports Windows.UI.Popups
+
+Module Vars
+
+    'The following public variables will be documented soon.
+
+    Public AccessWhat As String = "their Amazon account"
+    Public AppName As String = Package.Current.DisplayName
+    Public Company As String = "Amazon, Inc."
+    Public CountryName As String() = {"United Kingdom", "United States", "Australia", "Brazil", "Canada", "China", "France", "Germany", "India", "Italy", "Japan", "Mexico", "Netherlands", "Spain"}
+    Public CountryURL As String() = {"co.uk", "com", "com.au", "com.br", "ca", "cn", "fr", "de", "in", "it", "co.jp", "com.mx", "nl", "es"}
+    Public CountryValue As String
+    ''' <summary>
+    ''' The current height
+    ''' </summary>
+    Public CurrentHeight As Double
+    Public CurrentWidth As Double
+    Public localSettings As Windows.Storage.ApplicationDataContainer = Windows.Storage.ApplicationData.Current.LocalSettings
+    Public PrivacyInfo As String = AppName & " Privacy" & vbCrLf & vbCrLf & "----------" & vbCrLf & vbCrLf & "No personal, or private, information of either you, or your device, is collected by this app." & vbCrLf & vbCrLf & "Neither is ANY information transmitted by this app."
+    Public AboutInfo As String = AppName & " is a UWP app for Windows 10 Mobile to allow the user to access " & AccessWhat & " from a single portal." & vbCrLf & vbCrLf & "Small memory footprint, open source, and forever free." & vbCrLf & vbCrLf & "This app is NOT associated in ANY way with " & Company
+    Public Stored_ID As Object = localSettings.Values("STORED_COUNTRY")
+    Public SetFullScreen As Object = localSettings.Values("FULLSCREEN")
+    Public StoredCountry As String
+    Public URLBuilder As String
+    Public view = ApplicationView.GetForCurrentView()
+
+    'LoadCountries loads the list of countries into the lstCountry listbox.
+    Public Function LoadCountries()
+        Return CountryName
+    End Function
+
+    Public Async Function displayMessageAsync(ByVal title As String, ByVal content As String, ByVal dialogType As String) As Task
+        Dim messageDialog = New MessageDialog(content, title)
+        If dialogType = "notification" Then
+        Else
+            messageDialog.Commands.Add(New UICommand("Yes", Nothing))
+            messageDialog.Commands.Add(New UICommand("No", Nothing))
+            messageDialog.DefaultCommandIndex = 0
+        End If
+        messageDialog.CancelCommandIndex = 1
+        Dim cmdResult = Await messageDialog.ShowAsync()
+        If cmdResult.Label = "Yes" Then
+            Application.Current.Exit()
+        End If
+    End Function
+
+End Module
